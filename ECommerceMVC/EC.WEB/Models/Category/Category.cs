85a82d2abae11dbb48853fd1b82159dda3a54233
Dipu Mohanto <dipu.mohanto@selise.ch>
2023-07-27 21:22:15 +0600

    23. Data Annotations

--- ECommerceMVC/EC.WEB/Models/Category/Category.cs
+++ ECommerceMVC/EC.WEB/Models/Category/Category.cs
@@ -6,7 +6,10 @@ namespace EC.WEB.Models.Category
     {
         [Key]
         public int Id { get; set; }
-        public string Name { get; set; }
-        public int DisplayOrder { get; set; }
+
+        [Required]
+        public required string Name { get; set; }
+
+        public int? DisplayOrder { get; set; }
     }
 }
