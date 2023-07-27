3e7b860d6f6dd48bcebb757c94111db2ea5d1513
Dipu Mohanto <dipu.mohanto@selise.ch>
2023-07-27 22:46:28 +0600

    32. Get All Categories

--- ECommerceMVC/EC.WEB/Controllers/CategoryController.cs
+++ ECommerceMVC/EC.WEB/Controllers/CategoryController.cs
@@ -1,11 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EC.WEB.Data;
+using EC.WEB.Models.Category;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace EC.WEB.Controllers
 {
     public class CategoryController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext dbContext;
+
+        public CategoryController(ApplicationDbContext dbContext)
         {
+            this.dbContext = dbContext;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<Category> categories = await dbContext.Categories.ToListAsync();
             return View();
         }
     }
