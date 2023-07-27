cfd11016f03b15f9e2b870e54500abcfaa730dfe
Dipu Mohanto <dipu.mohanto@selise.ch>
2023-07-27 21:41:05 +0600

    26. Setup ApplicationDbContext

---
+++ ECommerceMVC/EC.WEB/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EC.WEB.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+    }
+}
