de840326bc33aae17c03f91fb1a4a28d4b2c91f5
Dipu Mohanto <dipu.mohanto@selise.ch>
2023-07-27 22:33:00 +0600

    29. Add Category Controller

--- ECommerceMVC/EC.WEB/Data/ApplicationDbContext.cs
+++ ECommerceMVC/EC.WEB/Data/ApplicationDbContext.cs
@@ -8,5 +8,7 @@ namespace EC.WEB.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
         }
+
+        public DbSet<Category> Categories { get; set; }
     }
 }
