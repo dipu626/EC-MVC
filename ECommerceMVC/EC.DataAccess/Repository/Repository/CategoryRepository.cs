649a655781f9eb25c13ed9a71bb53093f48fddd4
Dipu Mohanto <dipu.mohanto@selise.ch>
2023-07-28 22:30:00 +0600

    72. Implement Category Repository

---
+++ ECommerceMVC/EC.DataAccess/Repository/Repository/CategoryRepository.cs
@@ -0,0 +1,32 @@
+﻿using EC.DataAccess.Data;
+using EC.DataAccess.Repository.IRepository;
+using EC.Models.Category;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EC.DataAccess.Repository.Repository
+{
+    internal class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public CategoryRepository(ApplicationDbContext dbContext) : base(dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public async Task SaveAsync()
+        {
+            await this.dbContext.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Category category)
+        {
+            dbContext.Categories.Update(category);
+        }
+    }
+}
