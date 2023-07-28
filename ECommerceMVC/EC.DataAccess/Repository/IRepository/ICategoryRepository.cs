cca99d4bc9249dc25a7fc729281adea18f64f46c
Dipu Mohanto <dipu.mohanto@selise.ch>
2023-07-28 22:25:53 +0600

    71. Implement ICategoryRepository

---
+++ ECommerceMVC/EC.DataAccess/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,10 @@
+﻿using EC.Models.Category;
+
+namespace EC.DataAccess.Repository.IRepository
+{
+    internal interface ICategoryRepository : IRepository<Category>
+    {
+        Task UpdateAsync(Category category);
+        Task SaveAsync();
+    }
+}
