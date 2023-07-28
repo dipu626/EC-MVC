559052445373bab0458f73c8d03ce902acc675b2
Dipu Mohanto <dipu.mohanto@selise.ch>
2023-07-28 21:49:28 +0600

    69. IRepository Interface

---
+++ ECommerceMVC/EC.DataAccess/Repository/IRepository/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq.Expressions;
+
+namespace EC.DataAccess.Repository.IRepository
+{
+    internal interface IRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetAsync(Expression<Func<T, bool>> filter);
+        Task AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task RemoveAsync(T entity);
+        Task RemoveRangeAsync(IEnumerable<T> entities);
+    }
+}
