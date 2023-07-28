f2d382dcc882429fba5082bec1bcb9ba3afa9bb7
Dipu Mohanto <dipu.mohanto@selise.ch>
2023-07-28 22:23:02 +0600

    70. Implement Repository Interface

--- ECommerceMVC/EC.DataAccess/Repository/IRepository/IRepository.cs
+++ ECommerceMVC/EC.DataAccess/Repository/IRepository/IRepository.cs
@@ -2,10 +2,10 @@
 
 namespace EC.DataAccess.Repository.IRepository
 {
-    internal interface IRepository<T> where T : class
+    public interface IRepository<T> where T : class
     {
         Task<IEnumerable<T>> GetAllAsync();
-        Task<T> GetAsync(Expression<Func<T, bool>> filter);
+        Task<T?> GetAsync(Expression<Func<T, bool>> filter);
         Task AddAsync(T entity);
         Task UpdateAsync(T entity);
         Task RemoveAsync(T entity);
---
+++ ECommerceMVC/EC.DataAccess/Repository/Repository/Repository.cs
@@ -0,0 +1,51 @@
+﻿using EC.DataAccess.Data;
+using EC.DataAccess.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+
+namespace EC.DataAccess.Repository.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext dbContext;
+        internal DbSet<T> dbSet;
+
+        public Repository(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+            this.dbSet = dbContext.Set<T>();
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            await dbSet.AddAsync(entity);
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            IQueryable<T> query = dbSet;
+            return await query.ToListAsync();
+        }
+
+        public async Task<T?> GetAsync(System.Linq.Expressions.Expression<Func<T, bool>> filter)
+        {
+            IQueryable<T> query = dbSet;
+            query = query.Where(filter);
+            return await query.FirstOrDefaultAsync();
+        }
+
+        public async Task RemoveAsync(T entity)
+        {
+            dbSet.Remove(entity);
+        }
+
+        public async Task RemoveRangeAsync(IEnumerable<T> entities)
+        {
+            dbSet.RemoveRange(entities);
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            dbSet.Update(entity);
+        }
+    }
+}
