9741bb83baa6a878816805f366e490846be7d817
Dipu Mohanto <dipu.mohanto@selise.ch>
2023-07-31 09:45:10 +0600

    86. Add Image URL to Product

--- ECommerceMVC/EC.Models/Product/Product.cs
+++ ECommerceMVC/EC.Models/Product/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace EC.Models.Product
 {
@@ -39,5 +40,7 @@ namespace EC.Models.Product
         [DisplayName("Price for 100+")]
         [Range(1, 1000)]
         public double Price100 { get; set; }
+
+        public string ImageUrl { get; set; }
     }
 }
